698188b43219fc5fb0bbd951259cdedaa01a0414
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-04 14:17:14 -0500

    Initial Commit

---
+++ ChartsExamples/ChartsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExamplesApp.swift
+//  ChartsExamples
+//
+//  Created by Yuzhou Zhang on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
