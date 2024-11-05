a2d77e74cd4a7a5d2ace26fbd055f4d0b989a3b1
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-04 19:24:47 -0500

    added style to second example

--- ChartsExamples/ExampleViews.swift
+++ ChartsExamples/ExampleViews.swift
@@ -28,11 +28,14 @@ struct ContentView: View {
 struct ExampleTwoView: View {
     let weekdays = Calendar.current.shortWeekdaySymbols
     let steps = [ 10531, 6019, 7200, 8311, 7403, 6503, 9230 ]
-
     var body: some View {
         Chart {
             ForEach(weekdays.indices, id: \.self) { index in
                 BarMark(x: .value("Day", weekdays[index]), y: .value("Steps", steps[index]))
+            }
+            .foregroundStyle(by: .value("Day", weekdays[index]))
+            .annotation {
+                Text("\(steps[index])")
             }
         }
     }
