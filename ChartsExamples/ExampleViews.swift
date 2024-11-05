ae220c0d45a16ecb3b7fa6b386cd7a8100ab4ddd
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-04 20:42:14 -0500

    fixed bug for chart by moving the view modifier from ForEach to BarMark

--- ChartsExamples/ExampleViews.swift
+++ ChartsExamples/ExampleViews.swift
@@ -32,10 +32,10 @@ struct ExampleTwoView: View {
         Chart {
             ForEach(weekdays.indices, id: \.self) { index in
                 BarMark(x: .value("Day", weekdays[index]), y: .value("Steps", steps[index]))
-            }
-            .foregroundStyle(by: .value("Day", weekdays[index]))
-            .annotation {
-                Text("\(steps[index])")
+                    .foregroundStyle(by: .value("Day", weekdays[index]))
+                    .annotation {
+                        Text("\(steps[index])")
+                    }
             }
         }
     }
